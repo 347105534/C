b0454c02397bf39c1ff680514d96398f4c6ccf77
347105534 <347105534@qq.com>
2020-11-04 23:35:33 +0800

    1

---
+++ 2020.11.4/main.cpp
@@ -0,0 +1,70 @@
+//你是天才吗？
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	while (scanf("%d ", &a)){
+//		if (a >= 140){
+//			printf("Genius");
+//		}
+//		break;
+//	}
+//	return 0;
+//}
+
+
+//完美成绩
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	while (scanf("%d ", &a)){
+//		if (a >= 90){
+//			printf("Perfect");
+//		}
+//		break;
+//	}
+//	return 0;
+//}
+//及格分数
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	while (scanf("%d ", &a) != EOF){
+//		if (a >= 60){
+//			printf("Pass\n");
+//		}if (a<60){
+//			printf("Fail");
+//		}
+//
+//	}
+//	return 0;
+//}
+//判断整数奇偶性
+//#include<stdio.h>
+//int main()
+//{
+//	int a;
+//	while (scanf("%d ", &a) != EOF){
+//		if (a % 2 == 0){
+//			printf("Even\n");
+//		}if (a % 2 != 0){
+//			printf("Odd\n");
+//		}
+//
+//	}
+//	return 0;
+//}
+//最高分数
+//#include<stdio.h>
+//int main(){
+//	int a, b, c;
+//	while (scanf("%d %d %d ", &a, &b, &c) != EOF){
+//		int m = a>b ? a : b;
+//		m = m>c ? m : c;
+//		printf("%d\n", m);
+//	}
+//
+//	return 0;
+//}
