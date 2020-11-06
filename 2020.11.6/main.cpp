f8ec428901a48a06314ac088eb0638fb356f44c7
347105534 <347105534@qq.com>
2020-11-06 23:43:23 +0800

    3

---
+++ 2020.11.6/main.cpp
@@ -0,0 +1,34 @@
+//金字塔图案
+//#include<stdio.h>
+//int main(){
+//	int a;
+//	while (scanf("%d\n", &a) != EOF){
+//		for (int i = 1; i <= a; ++i){
+//			for (int m = 1; m <= a - i; ++m){
+//				printf(" ");
+//			}for (int n = 1; n <= i; ++n){
+//				printf("* ");
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
+//反转金字塔图案
+//#include<stdio.h>
+//int main(){
+//	int a;
+//	while (scanf("%d\n", &a) != EOF){
+//		for (int i = 1; i <= a; ++i){
+//			for (int m = 0; m <= i - 2; ++m){
+//				printf(" ");
+//			}for (int n = a; n >= i; --n){
+//				printf("* ");
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
