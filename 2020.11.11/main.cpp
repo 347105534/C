860724c6f47e7740ac9ff1468d7601fb0bc2fee2
347105534 <347105534@qq.com>
2020-11-11 23:16:31 +0800

    2020.11.11

---
+++ 2020.11.11/main.cpp
@@ -0,0 +1,15 @@
+//µÇÂ½ÑéÖ¤
+//#include <stdio.h>
+//#include <string.h>
+//
+//int main()
+//{
+//	char a[20], b[20];
+//	while (scanf("%s %s", a, b) != EOF)
+//	{
+//		if (strcmp(a, "admin") == 0 && strcmp(b, "admin") == 0)
+//			printf("Login Success!");
+//		else
+//			printf("Login Fail!");
+//	}
+//}
